06b2f0021a062500501e4cb9d0ca9a2fca986890
henriqtorresl <henriquetlandin@gmail.com>
2024-02-05 21:40:12 -0300

    pagina inicial parcialmente concluida

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   @Input() classeItens!: string;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  // arrumar essa lógica
+  ativarBotao(rota: any): string {
+    if (rota == 'home') {
+      return 'activate';
+    } else {
+      return '';
+    }
+  }
 }
