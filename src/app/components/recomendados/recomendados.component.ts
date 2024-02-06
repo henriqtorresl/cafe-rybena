25df8e353b2f54a277abce3c47f76365de6be02d
henriqtorresl <henriquetlandin@gmail.com>
2024-02-06 19:52:18 -0300

    logica dos cards concluida

--- src/app/components/recomendados/recomendados.component.ts
+++ src/app/components/recomendados/recomendados.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recomendados',
   templateUrl: './recomendados.component.html',
   styleUrls: ['./recomendados.component.scss']
 })
-export class RecomendadosComponent {
+export class RecomendadosComponent implements OnInit {
+
+  constructor(
+    private elementRef: ElementRef
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  proximo(): void {
+    const elem = this.elementRef.nativeElement.children[1].children[1];
+    const item = elem.getElementsByClassName('item');
+
+    elem.append(item[0]);
+  }
 
 }
