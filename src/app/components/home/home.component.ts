c7d26041e059779c3ec269ab1dca2a7aedbaaad2
henriqtorresl <henriquetlandin@gmail.com>
2024-02-28 16:09:52 -0300

    home page finalizada no mobile

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { CatalogoComponent } from '../catalogo/catalogo.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -10,18 +11,25 @@ import { MatDialog } from '@angular/material/dialog';
 export class HomeComponent implements OnInit {
 
   constructor(
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private snackbar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
     
   }
 
-  abrirCatalogo() {
+  abrirCatalogo(): void {
     this.dialog.open(CatalogoComponent, {
       width: '850px'
     });
   }
 
+
+  buy(): void {
+    this.snackbar.open('Não é possível fazer pedidos no momento!', 'OK', {
+      duration: 3000
+    });
+  }
 
 }
